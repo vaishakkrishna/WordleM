15f188e0237e79f5ed45e37be83913dc7c782cec
vaishakkrishna <vaishak.krishna@berkeley.edu>
2022-07-06 16:49:22 -0400

    positioning changes

--- src/pages/Helper.jsx
+++ src/pages/Helper.jsx
@@ -3,7 +3,7 @@ import Grid from "../components/Grid";
 import "./styles.css"
 function Helper() {
   return (
-    <div className="mx-5">
+    <div >
       <h1 className="my-5 center">Better Wordle.</h1>
       <Grid width="5" height="6" type="helper" />
     </div>
